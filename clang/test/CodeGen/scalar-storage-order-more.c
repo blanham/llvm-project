177564b763980232c633fce2432adf3c4a0f3e5e
Bryce Lanham <blanham@gmail.com>
2025-08-15 17:43:17 +0000

    [clang][CodeGen][tests] Complete endian matrix with little-endian attributed types on big-endian target

--- clang/test/CodeGen/scalar-storage-order-more.c
+++ clang/test/CodeGen/scalar-storage-order-more.c
@@ -13,6 +13,11 @@ typedef unsigned char __attribute__((scalar_storage_order("big-endian"))) be8_t;
 
 // Little-endian storage (matches x86_64) should not introduce bswap on LE but will on BE.
 typedef unsigned int __attribute__((scalar_storage_order("little-endian"))) le32_t;
+typedef unsigned short __attribute__((scalar_storage_order("little-endian"))) le16_t;
+typedef unsigned long long __attribute__((scalar_storage_order("little-endian"))) le64_t;
+typedef float __attribute__((scalar_storage_order("little-endian"))) lefloat;
+typedef double __attribute__((scalar_storage_order("little-endian"))) ledouble;
+typedef unsigned char __attribute__((scalar_storage_order("little-endian"))) le8_t; // width 8 -> no swap
 
 be16_t load16(be16_t *p) { // LE-LABEL: @load16
   // LE: call i16 @llvm.bswap.i16
@@ -66,4 +71,66 @@ void store_le32(le32_t *p, le32_t v) { // LE-LABEL: @store_le32
   // LE-NOT: call i32 @llvm.bswap.i32
   // BE: call i32 @llvm.bswap.i32
   *p = v; // BE-LABEL: @store_le32
+}
+
+// Additional little-endian storage types (mismatch on BE target) to complete matrix.
+
+le16_t load_le16(le16_t *p) { // LE-LABEL: @load_le16
+  // LE-NOT: call i16 @llvm.bswap.i16
+  // BE: call i16 @llvm.bswap.i16
+  return *p; // BE-LABEL: @load_le16
+}
+
+void store_le16(le16_t *p, le16_t v) { // LE-LABEL: @store_le16
+  // LE-NOT: call i16 @llvm.bswap.i16
+  // BE: call i16 @llvm.bswap.i16
+  *p = v; // BE-LABEL: @store_le16
+}
+
+le64_t load_le64(le64_t *p) { // LE-LABEL: @load_le64
+  // LE-NOT: call i64 @llvm.bswap.i64
+  // BE: call i64 @llvm.bswap.i64
+  return *p; // BE-LABEL: @load_le64
+}
+
+void store_le64(le64_t *p, le64_t v) { // LE-LABEL: @store_le64
+  // LE-NOT: call i64 @llvm.bswap.i64
+  // BE: call i64 @llvm.bswap.i64
+  *p = v; // BE-LABEL: @store_le64
+}
+
+lefloat load_leF(lefloat *p) { // LE-LABEL: @load_leF
+  // LE-NOT: call i32 @llvm.bswap.i32
+  // BE: call i32 @llvm.bswap.i32
+  return *p; // BE-LABEL: @load_leF
+}
+
+void store_leF(lefloat *p, lefloat v) { // LE-LABEL: @store_leF
+  // LE-NOT: call i32 @llvm.bswap.i32
+  // BE: call i32 @llvm.bswap.i32
+  *p = v; // BE-LABEL: @store_leF
+}
+
+ledouble load_leD(ledouble *p) { // LE-LABEL: @load_leD
+  // LE-NOT: call i64 @llvm.bswap.i64
+  // BE: call i64 @llvm.bswap.i64
+  return *p; // BE-LABEL: @load_leD
+}
+
+void store_leD(ledouble *p, ledouble v) { // LE-LABEL: @store_leD
+  // LE-NOT: call i64 @llvm.bswap.i64
+  // BE: call i64 @llvm.bswap.i64
+  *p = v; // BE-LABEL: @store_leD
+}
+
+le8_t load_le8(le8_t *p) { // LE-LABEL: @load_le8
+  // LE-NOT: call i8 @llvm.bswap
+  // BE-NOT: call i8 @llvm.bswap
+  return *p; // BE-LABEL: @load_le8
+}
+
+void store_le8(le8_t *p, le8_t v) { // LE-LABEL: @store_le8
+  // LE-NOT: call i8 @llvm.bswap
+  // BE-NOT: call i8 @llvm.bswap
+  *p = v; // BE-LABEL: @store_le8
 }
